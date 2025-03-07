4eb6f9f5b2f61af039afa43843b479d8b7948067
Minegem <104948247+Minegeem@users.noreply.github.com>
2025-03-07 16:48:04 +0100

    Delete DM_Algo directory

--- DM_Algo/greffe.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GREFFE_H__
-#define __GREFFE_H__
-#include "arbres_binaires.h"
-
-int copie(Arbre *dest, Arbre source);
-int expansion(Arbre *A, Arbre B);
-int expansion_aux(Arbre *G, Arbre A);
-#endif
